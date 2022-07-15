136e0acd5abd6c6018f655836e71705e450a0fd2
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-07-15 09:08:12 +0530

    Destini app

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
+    var story = StoryBrain()
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        //updateUI()
+        
 
     }
-
+   
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        story.nextStory()
+        storyLabel.text = story.getTitle()
+        choice1Button.setTitle(story.getChoice1(), for: .normal)
+        choice2Button.setTitle(story.getChoice2(), for: .normal)
+        
+        
+        
+    }
+    
 
 }
 
--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -7,5 +7,35 @@
 //
 
 import Foundation
-
+struct StoryBrain{
+    
+    let story = [ Story(title: "You see a fork on the Road", choice1: "Take a left", choice2:"Take a right"),
+    Story(title: "You see a tiger", choice1: "shout for help", choice2: "Play a dead"),
+    Story(title: "You find a treasure chest", choice1: "Open it ", choice2: "Check for traps")]
+    
+    var storyNumber = 0
+    
+    mutating func nextStory(){
+        if storyNumber + 1  < story.count{
+            storyNumber += 1
+            
+        }
+        else{
+            storyNumber = 0
+            
+        }
+    }
+    
+    func getTitle()->String{
+        return story[storyNumber].title
+    }
+    
+    func getChoice1()->String{
+        return story[storyNumber].choice1
+    }
+    
+    func getChoice2()->String{
+        return story[storyNumber].choice2
+    }
+}
 
